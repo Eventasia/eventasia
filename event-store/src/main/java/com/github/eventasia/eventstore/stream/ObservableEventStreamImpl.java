c24158177b8eb76c3b4e2198a7b0f43c66d723a2
Fabio Esposito <fabio.esposito@gmail.com>
2016-08-26 17:43:15 -0300

    add conditional annotation

--- event-store/src/main/java/com/github/eventasia/eventstore/stream/ObservableEventStreamImpl.java
+++ event-store/src/main/java/com/github/eventasia/eventstore/stream/ObservableEventStreamImpl.java
@@ -1,11 +1,13 @@
 package com.github.eventasia.eventstore.stream;
 
 import com.github.eventasia.framework.Event;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.stereotype.Component;
 
 import java.util.Observable;
 
 @Component
+@ConditionalOnMissingBean(EventStream.class)
 public class ObservableEventStreamImpl<E extends Event> extends Observable implements EventStream<E> {
 
     public void postEvent(E event) {

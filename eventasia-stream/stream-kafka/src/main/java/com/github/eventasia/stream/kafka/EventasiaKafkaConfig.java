97a6a4a78fc7472671bf0f8eae8d0150d3356102
Fabio Esposito <fabio.esposito@gmail.com>
2016-09-15 22:39:43 -0300

    renaming modules

---
+++ eventasia-stream/stream-kafka/src/main/java/com/github/eventasia/stream/kafka/EventasiaKafkaConfig.java
@@ -0,0 +1,100 @@
+package com.github.eventasia.stream.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.internals.DefaultPartitioner;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@SpringBootApplication
+@EnableKafka
+public class EventasiaKafkaConfig {
+
+    @Value("${eventasia.kafka.broker-list}")
+    private String brokerList;
+
+    @Value("${eventasia.kafka.topic}")
+    private String defaultTopic;
+
+    @Value("${eventasia.kafka.application-group}")
+    private String applicationGroup;
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<Integer, String>
+    kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<Integer, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<Integer, String> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs());
+    }
+
+    @Bean
+    public Map<String, Object> consumerConfigs() {
+        //FIXME: 12factorize
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, getBrokerList());
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, getApplicationGroup());
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "15000");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return props;
+    }
+
+    @Bean
+    public ProducerFactory<Integer, String> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    @Bean
+    public Map<String, Object> producerConfigs() {
+        //FIXME: 12factorize
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, getBrokerList());
+        props.put(ProducerConfig.RETRIES_CONFIG, 0);
+        //props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+        //props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+        //props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
+        props.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, DefaultPartitioner.class);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return props;
+    }
+
+    @Bean
+    public KafkaTemplate<Integer, String> kafkaTemplate() {
+
+        KafkaTemplate kafkaTemplate = new KafkaTemplate<>(producerFactory());
+        kafkaTemplate.setDefaultTopic(getDefaultTopic());
+
+        return kafkaTemplate;
+    }
+
+    public String getBrokerList() {
+        return brokerList;
+    }
+
+    public String getDefaultTopic() {
+        return defaultTopic;
+    }
+
+    public String getApplicationGroup() {
+        return applicationGroup;
+    }
+}

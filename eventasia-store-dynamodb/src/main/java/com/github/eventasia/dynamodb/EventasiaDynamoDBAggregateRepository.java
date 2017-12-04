5c7daece0d7df5d088a18e527b6e15be23cb764c
leonardo2204 <ferrari2204_5@hotmail.com>
2017-12-04 20:46:58 -0200

    fix get method

--- eventasia-store-dynamodb/src/main/java/com/github/eventasia/dynamodb/EventasiaDynamoDBAggregateRepository.java
+++ eventasia-store-dynamodb/src/main/java/com/github/eventasia/dynamodb/EventasiaDynamoDBAggregateRepository.java
@@ -3,6 +3,25 @@ package com.github.eventasia.dynamodb;
 import com.github.eventasia.eventstore.repository.ReadWriteAggregateRepository;
 import com.github.eventasia.framework.Aggregate;
 
-public interface EventasiaDynamoDBAggregateRepository<A extends Aggregate> extends ReadWriteAggregateRepository<A> {
+import java.util.UUID;
 
+public class EventasiaDynamoDBAggregateRepository<A extends Aggregate> implements ReadWriteAggregateRepository<A> {
+
+    private final EventasiaDynamoDBConfig config;
+    private final Class clazz;
+
+    public EventasiaDynamoDBAggregateRepository(EventasiaDynamoDBConfig config, Class clazz) {
+        this.config = config;
+        this.clazz = clazz;
+    }
+
+    @Override
+    public void save(A aggregate) {
+        config.getMapper().save(aggregate);
+    }
+
+    @Override
+    public A get(UUID uuid) {
+        return (A) config.getMapper().load(clazz, uuid);
+    }
 }
